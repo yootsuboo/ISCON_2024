390cfa6817d5b5a207fe2d956a811ac1d70908c7
ootsubo1192 <ootubo_stock@yahoo.co.jp>
2024-03-16 16:08:42 +0900

    fix s3 key

--- terraform/vpc/terraform_backend.tf
+++ terraform/vpc/terraform_backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "iscon-2024-terraform-state"
     region  = "ap-northeast-1"
-    key     = "aws/s3"
+    key     = "vpc/terraform.tfstate"
     encrypt = true
   }
 }
