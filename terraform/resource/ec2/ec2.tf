b4739e796c9991d4f326e150ebdae211c42d528f
ootsubo1192 <ootubo_stock@yahoo.co.jp>
2024-03-17 06:58:05 +0900

    add player instance security group

--- terraform/resource/ec2/ec2.tf
+++ terraform/resource/ec2/ec2.tf
@@ -1,14 +1,18 @@
+# -----------------
+# EC2
+# -----------------
+# player instance
 module "ec2_instance_player" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "5.6.0"
 
   name = "${local.prefix}-player"
 
-  instance_type               = "c6i.large"
+  instance_type               = "t3.large"
   ami                         = "ami-0d92a4724cae6f07b"
-  key_name                    = "devio-stg-key-pair"
+  key_name                    = "iscon_2024_key"
   monitoring                  = false
-  vpc_security_group_ids      = ["sg-00a8bbca60de63df1"]
+  vpc_security_group_ids      = [aws_security_group.sg_player.id]
   subnet_id                   = local.public_subnet_id_1a
   associate_public_ip_address = true
   ebs_optimized               = true
