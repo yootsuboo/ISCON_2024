a6d45bbf9e9ecf03d08487ebde7c31900c4007f2
ootsubo1192 <ootubo_stock@yahoo.co.jp>
2024-03-26 06:39:51 +0900

    ベンチマークインスタンスをコメントアウト

--- terraform/resource/ec2/ec2.tf
+++ terraform/resource/ec2/ec2.tf
@@ -20,20 +20,20 @@ module "ec2_instance_player" {
 }
 
 # benchmaker instance
-module "ec2_instance_benchmaker" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "5.6.0"
+# module "ec2_instance_benchmaker" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+#   version = "5.6.0"
 
-  name = "${local.prefix}-player"
+#   name = "${local.prefix}-player"
 
-  instance_type               = "c7a.medium"
-  ami                         = "ami-015450874c83c16e5"
-  key_name                    = "iscon_2024_key"
-  monitoring                  = false
-  vpc_security_group_ids      = [aws_security_group.sg_player.id]
-  subnet_id                   = local.public_subnet_id_1a
-  associate_public_ip_address = true
-  ebs_optimized               = true
-  enable_volume_tags          = true
-}
+#   instance_type               = "c7a.medium"
+#   ami                         = "ami-015450874c83c16e5"
+#   key_name                    = "iscon_2024_key"
+#   monitoring                  = false
+#   vpc_security_group_ids      = [aws_security_group.sg_player.id]
+#   subnet_id                   = local.public_subnet_id_1a
+#   associate_public_ip_address = true
+#   ebs_optimized               = true
+#   enable_volume_tags          = true
+# }
 
